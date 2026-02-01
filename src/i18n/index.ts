7af74946357669e694db59990738a53c2bd1e534
Essam <166946613+essamanalyst@users.noreply.github.com>
2026-02-01 04:49:44 +0200

    Add files via upload

---
+++ src/i18n/index.ts
@@ -0,0 +1,3 @@
+export { LanguageProvider, useLanguage, LanguageSwitcher } from './LanguageContext';
+export { translations, getTranslation, getTranslationWithVars } from './translations';
+export type { Language, TranslationKey } from './translations';
